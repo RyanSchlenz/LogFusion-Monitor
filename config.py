089b367e4156c308320cb978f2b3a4d0a0753913
Ryan Schlenz <96892298+RyanSchlenz@users.noreply.github.com>
2023-10-02 09:00:44 -0600

    Config file

---
+++ config.py
@@ -0,0 +1,12 @@
+LOG_FILE_PATHS = ['path/to/example.json', 'path/to/example.json']
+CSV_FILE_PATH = 'path/to/example.csv'
+JSON_FILE_PATH = 'path/to/matches.json_example'
+OUTPUT_FILE_PATH = 'path/to/example/csv_matches'
+LOG_FILE_PATH = 'path/to/example/activity.log'
+LOG_DIRECTORY = 'path/to/example/logs'
+
+
+CHECK_INTERVAL_SECONDS = 60  
+
+
+
